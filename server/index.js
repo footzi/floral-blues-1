62becda50247e38d3953f9f9a5abe28f2660b58d
Vladislav Manakov <footzi@mail.ru>
2018-11-27 20:43:29 +0300

    Изменяет server.js для тестового деплоя

--- server/index.js
+++ server/index.js
@@ -1,23 +1,8 @@
-const express = require('express');
-const twig = require('twig');
-const path = require('path');
-const config = require('./config');
-const clientRoute = require('../api/routes/client.js');
-
+const express = require("express");
 const app = express();
 
-// отдаем статичные файлы
-app.use(express.static(path.join(__dirname, '/../client')));
-
-// подключаем шаблонизатор, и нацелеваем на views
-app.set('views', path.join(__dirname + '/../client/src/pages'));
-app.set('view engine', 'twig');
-app.set('twig options', {strict_variables: false});
-
-// Обработка запросов
-app.use('/', clientRoute);
-
-// Запуск сервера
-app.listen(config.PORT, () => {
-    console.log(`Example app listening on port ${config.PORT}!`);
+app.get("/", (req, res) => {
+    res.send({ hello: "world" });
 });
+
+app.listen(process.env.PORT || 3000);
